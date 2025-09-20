c664c8039e797f818223de8c3c1d0941d9e603a2
Trend Nest <nesttrend30@gmail.com>
2025-09-20 11:46:08 +0000

    Are all the api's secured as i will push it to github

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -5,13 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-2777926424-9180e",
-  "appId": "1:357027570341:web:2e3525103a89735f53634c",
-  "storageBucket": "studio-2777926424-9180e.appspot.com",
-  "apiKey": "AIzaSyA972aE5Y8_KWR9CThyeSQBZimepxOtLWQ",
-  "authDomain": "studio-2777926424-9180e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "357027570341"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
